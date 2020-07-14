54c2f6bd4ab62327f1b7da4da5f0cf453c8650fe
xiaoqiu <32670876@qq.com>
2020-07-15 06:47:52 +0800

    主干更新2

--- MyShop2/src/main/java/com/neusoft/Demo1/Test1.java
+++ MyShop2/src/main/java/com/neusoft/Demo1/Test1.java
@@ -3,6 +3,8 @@ package com.neusoft.Demo1;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("dd");
+        System.out.println("创建分支");
+        System.out.println("主干更新2");
     }
 
 }
