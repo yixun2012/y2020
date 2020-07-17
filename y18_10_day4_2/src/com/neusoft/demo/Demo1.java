d8b2ac1300e70238ab406e43a984432eeda2611a
xiaoqiu <32670876@qq.com>
2020-07-17 09:42:59 +0800

    主线 冲突1

--- y18_10_day4_2/src/com/neusoft/demo/Demo1.java
+++ y18_10_day4_2/src/com/neusoft/demo/Demo1.java
@@ -4,5 +4,6 @@ public class Demo1 {
     public static void main(String[] args) {
         System.out.println("版本1");
         System.out.println("版本2");
+        System.out.println("冲突1");
     }
 }
